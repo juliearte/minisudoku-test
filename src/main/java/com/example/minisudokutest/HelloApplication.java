ec0c2e61e5bef8c7b0ff3d7b1db8d05e62f380a5
Julieta Arteta <arteta.aura@correounivalle.edu.co>
2025-04-03 16:47:09 -0500

    Changes

--- src/main/java/com/example/minisudokutest/HelloApplication.java
+++ src/main/java/com/example/minisudokutest/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
+        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        stage.setTitle("Que paNSO raza");
+        stage.setScene(scene);
         stage.show();
     }
 
